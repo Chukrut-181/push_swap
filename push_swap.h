5e6f4100bbc44e7ea93ca747c4f9909e293296af
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2024-08-21 10:06:26 +0200

    Added rotate command.c

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:29:41 by igchurru          #+#    #+#             */
-/*   Updated: 2024/08/21 09:57:14 by igchurru         ###   ########.fr       */
+/*   Updated: 2024/08/21 10:05:10 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,5 +90,11 @@ void			ss(t_stack_node	**a, t_stack_node	**b);
 void			push(t_stack_node **dest, t_stack_node **src);
 void			pa(t_stack_node **a, t_stack_node **b);
 void			pb(t_stack_node **b, t_stack_node **a);
+
+//	ROTATE_COMMAND.C
+void			rotate(t_stack_node **stack);
+void			ra(t_stack_node **a);
+void			rb(t_stack_node **b);
+void			rr(t_stack_node **a, t_stack_node **b);
 
 #endif
