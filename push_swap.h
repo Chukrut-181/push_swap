e5a45f39841959105cb4dd1aa7541cb4120ea3ec
Ignacio Churruca Fernandez De Tejada <igchurru@c4r7s2.42urduliz.com>
2024-09-09 09:45:38 +0200

    Makefile polishing and comments.

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: igchurru <igchurru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 10:29:41 by igchurru          #+#    #+#             */
-/*   Updated: 2024/08/23 08:32:01 by igchurru         ###   ########.fr       */
+/*   Updated: 2024/09/09 09:44:48 by igchurru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 
 /*	T_STACK_NODE STRUCTURE 
 
-This is thee data structure that will form the stack to be sorted.
+This is the data structure that will form the stack to be sorted.
 Contains all necessary metadata to evaluate the optimal move and sort
 the initial stack following the "game rules".
 
